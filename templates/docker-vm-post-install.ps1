e8322861b04587eb823f9c0bb95f61183dfda095
David Okeyode <asegunlolu@hotmail.com>
2023-07-07 18:20:19 +0100

    modified c1 template

--- templates/docker-vm-post-install.ps1
+++ templates/docker-vm-post-install.ps1
@@ -1,12 +1,10 @@
 $env:chocolateyVersion = '1.4.0'
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install git vscode azure-cli az.powershell nodejs.install dotnet-6.0-sdk dotnet-sdk dotnetcore-sdk googlechrome setdefaultbrowser docker-desktop win-no-annoy -y
+choco install git vscode azure-cli az.powershell nodejs.install dotnet-6.0-sdk dotnet-sdk dotnetcore-sdk googlechrome setdefaultbrowser win-no-annoy -y
 choco install vcredist-all -y
 choco install miniconda3 --version=4.8.3 --params="'/AddToPath:1 /InstallationType:AllUsers /RegisterPython:1'" -y
 choco install microsoft-windows-terminal -y
-
-Add-LocalGroupMember -Group "docker-users" -Member "azureuser"
 
 code --install-extension ms-dotnettools.csharp --force
 code --install-extension ms-python.python --force
